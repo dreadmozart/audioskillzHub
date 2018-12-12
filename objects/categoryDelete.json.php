41f9971ea25556455c7fe1cfe8385320a6d666c1
Dereck-npo-gurova <dreadmozart@gmail.com>
2018-12-12 18:52:22 +0300

    init

---
+++ objects/categoryDelete.json.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: application/json');
+global $global, $config;
+if(!isset($global['systemRootPath'])){
+    require_once '../videos/configuration.php';
+}
+require_once $global['systemRootPath'] . 'objects/user.php';
+if(!Category::canCreateCategory()){
+    die('{"error":"'.__("Permission denied").'"}');
+}
+require_once 'category.php';
+$obj = new Category($_POST['id']);
+echo '{"status":"'.$obj->delete().'"}';
